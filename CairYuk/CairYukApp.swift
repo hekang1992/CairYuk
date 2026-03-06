a0d3f2bbea3e12f10a7c436ca3789a2a88e59622
hekang1992 <hekang19921111@hotmail.com>
2026-03-06 10:15:03 +0800

    first commit

---
+++ CairYuk/CairYukApp.swift
@@ -0,0 +1,17 @@
+//
+//  CairYukApp.swift
+//  CairYuk
+//
+//  Created by hekang on 2026/3/6.
+//
+
+import SwiftUI
+
+@main
+struct CairYukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
