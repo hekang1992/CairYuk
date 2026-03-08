5553deefb13380e9ed3a556ce5a9af2e25b3b69c
hekang1992 <hekang19921111@hotmail.com>
2026-03-08 21:08:03 +0800

    f

---
+++ CairYuk/Base/LaunchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchViewController.swift
+//  CairYuk
+//
+//  Created by hekang on 2026/3/8.
+//
+
+import UIKit
+
+class LaunchViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
